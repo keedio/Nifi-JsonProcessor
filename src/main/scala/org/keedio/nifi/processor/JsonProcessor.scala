37441f0319778d4fe609349bf367fc101c9cf92b
Luis Lázaro <lalazaro@keedio.com>
2015-09-21 12:54:43 +0200

    iNT-63 custom processor

--- src/main/scala/org/keedio/nifi/processor/JsonProcessor.scala
+++ src/main/scala/org/keedio/nifi/processor/JsonProcessor.scala
@@ -1,11 +1,20 @@
 package org.keedio.nifi.processor
 
+import java.io.{IOException, InputStream, OutputStream}
+import java.util.concurrent.atomic.AtomicReference
+
+import com.jayway.jsonpath.JsonPath
+import org.apache.commons.io.IOUtils
 import org.apache.nifi.annotation.documentation.{CapabilityDescription, Tags}
 import org.apache.nifi.components.PropertyDescriptor
+import org.apache.nifi.flowfile.FlowFile
+import org.apache.nifi.logging.ProcessorLog
 import org.apache.nifi.processor._
+import org.apache.nifi.processor.exception.ProcessException
+import org.apache.nifi.processor.io.{InputStreamCallback, OutputStreamCallback}
 import org.apache.nifi.processor.util.StandardValidators
 
-import scala.collection.mutable.{ListBuffer, Set}
+import scala.collection.immutable.HashSet
 
 
 /**
@@ -16,17 +25,62 @@ import scala.collection.mutable.{ListBuffer, Set}
 
 @Tags("Json", "JsonProcessor")
 @CapabilityDescription("Fetch value from json path")
-class JsonProcessor extends AbstractProcessor{
+class JsonProcessor extends AbstractProcessor {
 
-    private val properties: ListBuffer[PropertyDescriptor] = ListBuffer()
-    private val relationships: Set[Relationship] = Set()
+    private val properties: List[PropertyDescriptor] = Nil
+    private val relationships: HashSet[Relationship] = HashSet.empty
 
+    /**
+     * Function init called at the start of Apache Nifi
+     * @param context
+     */
+    override def init(context: ProcessorInitializationContext): Unit = {
+        JsonProcessor.JSON_PATH :: properties
+        relationships.+(JsonProcessor.SUCCESS)
+    }
 
+    /**
+     * Function onTrigger called wheneve a flowFile is passed to the processor.
+     * @param context
+     * @param session
+     */
+    @throws(classOf[ProcessException])
+    override def onTrigger(context: ProcessContext, session: ProcessSession): Unit = {
+        val log: ProcessorLog = this.getLogger
+        val value: AtomicReference[String] = new AtomicReference[String]()
+        var flowFile: FlowFile = session.get()
 
-    override def init(context: ProcessorInitializationContext): Unit = ???
-    override def onTrigger(context: ProcessContext, session: ProcessSession): Unit = ???
-    override def getRelationships(): Set[Relationship] = ???
-    override def getSupportedPropertyDescriptors(): List[PropertyDescriptor] = ???
+        session.read(flowFile, new InputStreamCallback {
+            @throws(classOf[IOException])
+            override def process(inputStream: InputStream): Unit = {
+                try {
+                    val json = IOUtils.toString(inputStream)
+                    val result = JsonPath.read(json, "$.hello")
+                    value.set(result)
+                } catch {
+                    case ex: IOException => ex.printStackTrace()
+                        log.error("Failed to read json string")
+                }
+            }
+        })
+        val results: String = value.get()
+        results.nonEmpty && results.isInstanceOf[NotNull] match {
+            case true => flowFile = session.putAttribute(flowFile, "match", results)
+            case false => println("error")
+        }
+
+        flowFile = session.write(flowFile, new OutputStreamCallback {
+            @throws(classOf[IOException])
+            override def process(outputStream: OutputStream): Unit =
+                outputStream.write(value.get().getBytes())
+        })
+        session.transfer(flowFile, JsonProcessor.SUCCESS)
+
+    }
+
+    override def getRelationships(): HashSet[Relationship] = relationships
+
+    override def getSupportedPropertyDescriptors(): List[PropertyDescriptor] = properties
 }
 
 object JsonProcessor {
