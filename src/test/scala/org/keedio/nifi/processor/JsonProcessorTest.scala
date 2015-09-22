64a556b1e065247909f93fc943b08f924a777034
Luis Lázaro <lalazaro@keedio.com>
2015-09-22 09:51:52 +0200

    iNT-63 ignore test

--- src/test/scala/org/keedio/nifi/processor/JsonProcessorTest.scala
+++ src/test/scala/org/keedio/nifi/processor/JsonProcessorTest.scala
@@ -1,32 +1,40 @@
 package org.keedio.nifi.processor
 
+import java.io.{ByteArrayInputStream, InputStream}
 
+import org.apache.commons.io.IOUtils
+import org.apache.nifi.util.{MockFlowFile, TestRunner, TestRunners}
+import org.junit.{Ignore, Test}
+import org.junit.Assert.assertTrue
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 /**
  * Created by luislazaro on 21/9/15.
  * lalazaro@keedio.com
  * Keedio
  */
+@Ignore
 class JsonProcessorTest {
 
-//    @Test
-//    def testOnTrigger(): Unit  = {
-//        val content: InputStream = new ByteArrayInputStream("{\"hola\":\"keedio mola\"}".getBytes())
-//        val a = new JsonProcessor
-//        val runner: TestRunner = TestRunners.newTestRunner(a)
-//        runner.setProperty(JsonProcessor.JSON_PATH, "$.hola")
-//        runner.enqueue(content)
-//        runner.run(1)
-//        runner.assertQueueEmpty()
-//
-//        val results0: mutable.Buffer[MockFlowFile] = runner.getFlowFilesForRelationship(JsonProcessor.SUCCESS).asScala
-//        val results: List[MockFlowFile] = results0.toList
-//        assertTrue("1 match", results.size == 1)
-//        val result: MockFlowFile = results(0)
-//        val resultValue = new String(runner.getContentAsByteArray(result))
-//        println("Match: " + IOUtils.toString(runner.getContentAsByteArray(result)))
-//
-//        result.assertAttributeEquals(JsonProcessor.MATCH_ATTR, "Keedio mola")
-//        result.assertContentEquals("Keedio mola")
-//    }
+    @Test
+    def testOnTrigger(): Unit  = {
+        val content: InputStream = new ByteArrayInputStream("{\"hola\":\"keedio mola\"}".getBytes())
+        val a = new JsonProcessor
+        val runner: TestRunner = TestRunners.newTestRunner(a)
+        runner.setProperty(JsonProcessor.JSON_PATH, "$.hola")
+        runner.enqueue(content)
+        runner.run(1)
+        runner.assertQueueEmpty()
+
+        val results0: mutable.Buffer[MockFlowFile] = runner.getFlowFilesForRelationship(JsonProcessor.SUCCESS).asScala
+        val results: List[MockFlowFile] = results0.toList
+        assertTrue("1 match", results.size == 1)
+        val result: MockFlowFile = results(0)
+        val resultValue = new String(runner.getContentAsByteArray(result))
+        println("Match: " + IOUtils.toString(runner.getContentAsByteArray(result)))
+
+        result.assertAttributeEquals(JsonProcessor.MATCH_ATTR, "Keedio mola")
+        result.assertContentEquals("Keedio mola")
+    }
 }
